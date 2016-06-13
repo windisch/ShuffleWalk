81a91c18b22dc3dbcb9c3beef0be26b5f9ecbb61
windisch <windisch@ovgu.de>
2016-06-13 08:20:04 +0200

    add methods for p-value computation

--- shuffleWalk.r
+++ shuffleWalk.r
@@ -3,9 +3,9 @@
 ########################################
 
 #Intial table (All columns need to have the same sum.)
-u=matrix(c(1,1,1,
-           1,1,1,
-           1,1,1),nrow=3,ncol=3,byrow=T)
+u=matrix(c(1,5,3,
+           2,7,7,
+           10,1,3),nrow=3,ncol=3,byrow=T)
 
 #How many steps?
 TT=100
@@ -40,22 +40,64 @@ ShuffleStep=function(u){
   return(u)     
 }
 
+#compute the rejection probability of MH-Algorihm
+metropolisRejection=function(u,v){
+#current state: u
+#new state: v
+    u=as.vector(u)
+    v=as.vector(v)
+    d=length(u)
+    p=1
+    for(i in 1:d){
+        if(u[i]!=v[i]){
+            p=p*factorial(u[i])/factorial(v[i])
+        }
+    }
+    return(1-min(1,p))
+}
+
+#Chi2 for independence
+chi2indep=function(u){
+    rS=rowSums(u)
+    r=length(rS)
+    cS=colSums(u)
+    c=length(cS)
+    n=sum(u)
+    X=0
+    for(i in 1:r){
+        for(j in 1:c){
+            MLE=rS[i]*cS[j]/n
+            X=X+(u[i,j]-MLE)^2/MLE
+            }
+        }
+    return(X)
+}
 
 #Mixed Shuffle Walk (print all TT tables obseved)
 MixedShuffle=function(u,TT){
   
-  tables=list()
-  tables[[1]]=u
+#  tables=list()
+#  tables[[1]]=u
+  Xu=chi2indep(u)
+  X=c(1)
   
   for (i in 1:TT){
     
-    walk=sample(c("D","S"),size=1)
     switch(sample(c("D","S"),size=1),
-        "D"={u=DiaconisStep(u)},
-        "S"={u=ShuffleStep(u)},
-            {print('default')})
+        "D"={v=DiaconisStep(u)},
+        "S"={v=ShuffleStep(u)})
     
-    tables[[(i+1)]]=u
+    #rejection probability
+    q=metropolisRejection(u,v)
+    if(sample(c(0,1),size=1,prob=c(q,1-q))==1){
+        #do not reject
+        u=v
+        }
+
+#    tables[[(i+1)]]=u
+    if(chi2indep(u)>=Xu){
+         X=c(X,1)
+    }
   }
-  return(tables)
+  return(sum(X)/TT)
 }
